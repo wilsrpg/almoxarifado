8ca9178b2d6de11d43096715842f690b75f07ff3
wilsrpg <wilson.rpg@hotmail.com>
2024-12-11 15:29:18 -0300

    resumi código

--- app/Http/Livewire/ListaDeItens.php
+++ app/Http/Livewire/ListaDeItens.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Livewire;
 
-use Illuminate\Database\Eloquent\Collection;
 use Livewire\Component;
 use Livewire\Attributes\On;
 
@@ -15,24 +14,17 @@ class ListaDeItens extends Component
   public function mount() {
     $this->enviados = array_pad($this->enviados, count($this->lista_de_itens), false);
   }
-  public function enviar($id_do_item) {
-    //$item = $this->lista_de_itens->find($id_do_item);
-    //$this->lista_de_itens->find($id_do_item)['enviado'] = true;
+  public function indice($id_do_item) {
     $ids = array_map(function($i){ return $i['id']; }, $this->lista_de_itens->toArray());
-    //$ids = array_map(function($i){ return $i['id']; }, $this->lista_de_itens);
-    $indice = array_search($id_do_item, $ids);
-    //$item['enviado'] = true;
+    return array_search($id_do_item, $ids);
+  }
+  public function enviar($id_do_item) {
+    $indice = $this->indice($id_do_item);
     $this->enviados[$indice] = true;
-    $item = $this->lista_de_itens[$indice];
-    //$this->lista_de_itens[0]['anotacoes'] = print_r(array_search($id_do_item, $ids), true);
-    $this->dispatch('adicionar-item', $item, $this->destino);
+    $this->dispatch('adicionar-item', $this->lista_de_itens[$indice], $this->destino);
   }
   #[On('item-removido')]
   public function reabilitar($id_do_item) {
-    $ids = array_map(function($i){ return $i['id']; }, $this->lista_de_itens->toArray());
-    $indice = array_search($id_do_item, $ids);
-    $this->enviados[$indice] = false;
-    //unset($this->lista_de_itens[$indice]['enviado']);
-    //$this->lista_de_itens->find($id_do_item)['enviado'] = false;
+    $this->enviados[$this->indice($id_do_item)] = false;
   }
 }
