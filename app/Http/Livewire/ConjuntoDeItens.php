bb1665278298c214c30f210da035c0d1fe58cde0
wilsrpg <wilson.rpg@hotmail.com>
2024-12-12 13:21:27 -0300

    melhorei interação entre lista e conjunto de itens

--- app/Http/Livewire/ConjuntoDeItens.php
+++ app/Http/Livewire/ConjuntoDeItens.php
@@ -15,11 +15,31 @@ class ConjuntoDeItens extends Component
   public function adicionar($item, $destino) {
     if ($this->nome == $destino)
       $this->itens_do_grupo[] = $item;
+    $this->dispatch('atualizar-itens-enviados',
+      array_map(function($i){ return $i['id']; }, $this->itens_do_grupo)
+    );
   }
+
   public function remover($id_do_item) {
     $this->itens_do_grupo = array_filter($this->itens_do_grupo,
       function($i) use($id_do_item) { return $i['id'] != $id_do_item; }
     );
-    $this->dispatch('item-removido', $id_do_item);
+    //$this->dispatch('item-removido', $id_do_item);
+    $this->dispatch('atualizar-itens-enviados',
+      array_map(function($i){ return $i['id']; }, $this->itens_do_grupo)
+    );
+  }
+
+  //public function hydrate() {
+  //  $this->dispatch('atualizar-itens-enviados',
+  //    array_map(function($i){ return $i['id']; }, $this->itens_do_grupo)
+  //  );
+  //}
+
+  #[On('obter-itens-enviados')]
+  public function atualizar_lista() {
+    $this->dispatch('atualizar-itens-enviados',
+      array_map(function($i){ return $i['id']; }, $this->itens_do_grupo)
+    );
   }
 }
--- app/Http/Livewire/ListaDeItens.php
+++ app/Http/Livewire/ListaDeItens.php
@@ -13,18 +13,29 @@ class ListaDeItens extends Component
 
   public function mount() {
     $this->enviados = array_pad($this->enviados, count($this->lista_de_itens), false);
+    $this->dispatch('obter-itens-enviados');
   }
+
   public function indice($id_do_item) {
     $ids = array_map(function($i){ return $i['id']; }, $this->lista_de_itens->toArray());
     return array_search($id_do_item, $ids);
   }
+
   public function enviar($id_do_item) {
     $indice = $this->indice($id_do_item);
     $this->enviados[$indice] = true;
     $this->dispatch('adicionar-item', $this->lista_de_itens[$indice], $this->destino);
   }
-  #[On('item-removido')]
-  public function reabilitar($id_do_item) {
-    $this->enviados[$this->indice($id_do_item)] = false;
+
+  //#[On('item-removido')]
+  //public function reabilitar($id_do_item) {
+  //  $this->enviados[$this->indice($id_do_item)] = false;
+  //}
+
+  #[On('atualizar-itens-enviados')]
+  public function atualizar($ids_dos_itens_do_grupo) {
+    for ($i=0; $i < count($this->lista_de_itens); $i++) {
+      $this->enviados[$i] = array_search($this->lista_de_itens[$i]['id'], $ids_dos_itens_do_grupo) !== false;
+    }
   }
 }
