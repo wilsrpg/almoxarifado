ef0e70aecae05a95198acade179a38ee4d372d74
wilsrpg <wilson.rpg@hotmail.com>
2024-12-17 01:35:23 -0300

    componentizei a parte dos itens

--- resources/views/movimentacoes/editar_movimentacao.blade.php
+++ resources/views/movimentacoes/editar_movimentacao.blade.php
@@ -2,7 +2,7 @@
 @section('titulo', 'Editando movimentação: '.$movimentacao->nome.' - Almoxarifado')
 @section('conteudo')
 
-<form action="/movimentacao/{{$movimentacao->id}}/atualizar" method="post">
+<form action="/movimentacao/{{$movimentacao->id}}/atualizar" method="post" onsubmit="checar_itens(event)">
   @csrf
   @method('PUT')
   <p>ID: {{$movimentacao->id}}</p>
@@ -10,118 +10,47 @@
   <p>Hora: <input type="time" name="hora" value="{{$movimentacao->hora}}"></p>
   <p>Responsável por entregar: <input name="quem_entregou" value="{{$movimentacao->quem_entregou}}"></p>
   <p>Responsável por receber: <input name="quem_recebeu" value="{{$movimentacao->quem_recebeu}}"></p>
-  <p>Tipo:
-    <select name="tipo" onchange="mudarTipo(event)" required>
-      <option value="Empréstimo" {{$movimentacao->tipo == 'Empréstimo' ? 'selected' : ''}}>Empréstimo</option>
-      <option value="Devolução" {{$movimentacao->tipo == 'Devolução' ? 'selected' : ''}}>Devolução</option>
-      <option value="Transferência" {{$movimentacao->tipo == 'Transferência' ? 'selected' : ''}}>Transferência</option>
-    </select>
-  </p>
-  <p>
-    <span style="vertical-align: top;">Itens: </span>
-    @if (count($itens))
-      <select id="itens" name="itens[]" multiple required>
-        @foreach ($itens as $item)
-          <option id="{{$item['_id']}}" value="{{$item['_id']}}"
-            {{--class="{{$item['disponivel'] ? 'disp' : 'ndis'}}
-              {{array_search($item['_id'], $movimentacao['itens']) !== false ? 'mov' : ''}}"--}}
-            class="<?php
-              echo $item['disponivel'] ? 'disp' : 'ndis';
-              if (array_search($item['_id'], $movimentacao['itens']) !== false)
-                echo ' mov';
-            ?>"
-            <?php
-              if (array_search($item['_id'], $movimentacao['itens']) !== false)
-                echo 'selected';
-              elseif ($item['disponivel'] && $movimentacao->tipo != 'Empréstimo')
-                echo 'disabled';
-              elseif (!$item['disponivel'] && $movimentacao->tipo == 'Empréstimo')
-                echo 'disabled';
-            ?>
-            {{--{{array_search($item['_id'], $movimentacao['itens']) !== false ? 'selected' : 
-            $item['disponivel'] && $movimentacao->tipo != 'Empréstimo' ? 'disabled' : 
-            !$item['disponivel'] && $movimentacao->tipo == 'Empréstimo' ? 'disabled' : ''}}--}}
-          >
-            {{array_search($item['_id'], $movimentacao['itens']) !== false ? '*' : ''}}
-            {{$item['nome']}}
-          </option>
-        @endforeach
-      </select>
-    @else
-      Não há itens cadastrados.
-    @endif
-    <span style="vertical-align: top;">Grupos: </span>
-    @if (count($grupos))
-      <select id="grupos" multiple onchange="atualizarItens(event)">
-        @foreach ($grupos as $grupo)
-          <option value="{{$grupo['id']}}">
-            {{$grupo['nome']}}
-          </option>
-        @endforeach
-      </select>
-      @foreach ($grupos as $grupo)
-        <input type="hidden" id="{{$grupo['_id']}}" value="{{implode(',', $grupo['itens'])}}">
-      @endforeach
-    @else
-      Não há grupos cadastrados.
-    @endif
-    <span style="vertical-align: top;">Movimentações: </span>
-    @if (count($movimentacoes))
-      <select id="movimentacoes" multiple onchange="atualizarItens(event)">
-        @foreach ($movimentacoes as $mov)
-          <option value="{{$mov['_id']}}">
-            {{date_format(date_create($mov['data']), 'd/m/Y') . ' ' . $mov['hora']}}
-          </option>
-        @endforeach
-      </select>
-      @foreach ($movimentacoes as $mov)
-        <input type="hidden" id="{{$mov['_id']}}" value="{{implode(',', $mov['itens'])}}">
-      @endforeach
-    @else
-      Não há movimentações registradas.
-    @endif
-  </p>
+  <livewire:tipo-da-movimentacao :tipo_da_movimentacao="$movimentacao->tipo"/>
+  <div style="display: flex">
+    <?php
+      $itens_do_conjunto = [];
+      foreach ($movimentacao->itens as $it)
+        $itens_do_conjunto[] = $itens->find($it);
+      //echo '<pre>';print_r($movimentacao->itens);die();
+    ?>
+    <livewire:conjunto-de-itens
+      :itens_do_conjunto="$itens_do_conjunto"
+      :nome="'itens-da-movimentacao'"
+      :name="'itens[]'"
+      :tipo_da_movimentacao="$movimentacao->tipo"
+    />
+    <livewire:lista-de-itens
+      :lista_de_itens="$itens"
+      :destino="'itens-da-movimentacao'"
+      :tipo_da_movimentacao="$movimentacao->tipo"
+    />
+    <livewire:lista-de-grupos
+      :lista_de_grupos="$grupos"
+      :lista_de_itens="$itens"
+      :destino="'itens-da-movimentacao'"
+      :tipo_da_movimentacao="$movimentacao->tipo"
+    />
+  </div>
   <p>
     <span style="vertical-align: top;">Anotações: </span>
     <textarea name="anotacoes">{{$movimentacao->anotacoes}}</textarea>
   </p>
   <input type="submit" value="Salvar">
+  <span id="erro" class="vermelho"></span>
 </form>
 
 <script>
-  function mudarTipo(e) {
-    document.getElementById('itens').disabled = e.target.value == '';
-    document.getElementById('grupos').disabled = e.target.value == '';
-    document.getElementById('movimentacoes').disabled = e.target.value == '';
-    if (e.target.value != '') {
-      itens = document.getElementById('itens').children;
-      if (e.target.value == 'Empréstimo')
-        for (let i = 0; i < itens.length; i++)
-          itens[i].disabled = itens[i].className.search('disp') < 0 && itens[i].className.search('mov') < 0;
-      else
-        for (let i = 0; i < itens.length; i++)
-          itens[i].disabled = itens[i].className.search('ndis') < 0 && itens[i].className.search('mov') < 0;
-    }
-  }
-
-  function atualizarItens(e) {
-    //console.log(e.target.children[0].selected);
-    let arr = []; //pra converter HTMLCollection em Array, pra usar forEach
-    for (let i = 0; i < e.target.children.length; i++) {
-      arr.push(e.target.children[i]);
-    }
-    //console.log(arr);
-    arr.forEach(op => {
-      if (document.getElementById(op.value).value) {
-        let ids_dos_itens = document.getElementById(op.value).value.split(',');
-        //console.log(ids_dos_itens);
-        if (ids_dos_itens.length)
-          ids_dos_itens.forEach(id => {
-            if (op.selected && !document.getElementById(id).disabled)
-              document.getElementById(id).selected = op.selected;
-          });
-      }
-    });
+  function checar_itens(e) {
+    e.preventDefault();
+    if (document.getElementsByName('itens[]').length == 0)
+      document.getElementById('erro').textContent = 'Inclua pelo menos um item.';
+    else
+      e.target.submit();
   }
 </script>
 
--- resources/views/movimentacoes/nova_movimentacao.blade.php
+++ resources/views/movimentacoes/nova_movimentacao.blade.php
@@ -2,127 +2,44 @@
 @section('titulo', 'Nova movimentação - Almoxarifado')
 @section('conteudo')
 
-<form action="/movimentacoes/criar" method="post">
+<form action="/movimentacoes/criar" method="post" onsubmit="checar_itens(event)">
+  @csrf
   <p>Data: <input type="date" name="data" value="<?php echo date('Y-m-d'); ?>"></p>
   <p>Hora: <input type="time" name="hora" value="<?php echo date('H:i'); ?>"></p>
   <p>Responsável por entregar: <input name="quem_entregou"></p>
   <p>Responsável por receber: <input name="quem_recebeu"></p>
-  <p>Tipo:
-    <select name="tipo" onchange="mudarTipo(event)" required>
-      <option default></option>
-      <option value="Empréstimo">Empréstimo</option>
-      <option value="Devolução">Devolução</option>
-      <option value="Transferência">Transferência</option>
-    </select>
-  </p>
-  <p>
-    <span style="vertical-align: top;">Itens: </span>
-    @if (count($itens))
-      <select id="itens" name="itens[]" multiple required disabled>
-        @foreach ($itens as $item)
-          <option id="{{$item['_id']}}" value="{{$item['_id']}}" class="{{$item['disponivel'] ? 'disp' : 'ndisp'}}">
-            {{$item['nome']}}
-          </option>
-        @endforeach
-      </select>
-    @else
-      Não há itens cadastrados.
-    @endif
-    <span style="vertical-align: top;">Grupos: </span>
-    @if (count($grupos))
-      <select id="grupos" multiple onchange="atualizarItens(event)" disabled>
-        @foreach ($grupos as $grupo)
-          <option value="{{$grupo['_id']}}">
-            {{$grupo['nome']}}
-          </option>
-        @endforeach
-      </select>
-      @foreach ($grupos as $grupo)
-        <input type="hidden" id="{{$grupo['_id']}}" value="{{implode(',', $grupo['itens'])}}">
-      @endforeach
-    @else
-      Não há grupos cadastrados.
-    @endif
-    <span style="vertical-align: top;">Movimentações: </span>
-    @if (count($movimentacoes))
-      <select id="movimentacoes" multiple onchange="atualizarItens(event)" disabled>
-        @foreach ($movimentacoes as $movimentacao)
-          <option value="{{$movimentacao['_id']}}">
-            {{date_format(date_create($movimentacao['data']), 'd/m/Y') . ' ' . $movimentacao['hora']}}
-          </option>
-        @endforeach
-      </select>
-      @foreach ($movimentacoes as $movimentacao)
-        <input type="hidden" id="{{$movimentacao['_id']}}" value="{{implode(',', $movimentacao['itens'])}}">
-      @endforeach
-    @else
-      Não há movimentações registradas.
-    @endif
-  </p>
+  <livewire:tipo-da-movimentacao />
+  <div style="display: flex">
+    <livewire:conjunto-de-itens
+      :nome="'itens-da-movimentacao'"
+      :name="'itens[]'"
+    />
+    <livewire:lista-de-itens
+      :lista_de_itens="$itens"
+      :destino="'itens-da-movimentacao'"
+      :em_movimentacao="true"
+    />
+    <livewire:lista-de-grupos
+      :lista_de_grupos="$grupos"
+      :lista_de_itens="$itens"
+      :destino="'itens-da-movimentacao'"
+    />
+  </div>
   <p>
     <span style="vertical-align: top;">Anotações: </span>
     <textarea name="anotacoes"></textarea>
   </p>
-  @csrf
   <input type="submit" value="Registrar">
+  <span id="erro" class="vermelho"></span>
 </form>
 
 <script>
-  function mudarTipo(e) {
-    itens = document.getElementById('itens');
-    //grupos = document.getElementById('grupos');
-    //movimentacoes = document.getElementById('movimentacoes');
-    itens.disabled = e.target.value == '';
-    document.getElementById('grupos').disabled = e.target.value == '';
-    document.getElementById('movimentacoes').disabled = e.target.value == '';
-    /*if (e.target.value == '') {
-      //itens.disabled = true;
-      //grupos.disabled = true;
-      //movimentacoes.disabled = true;
-    } else {
-      //itens.disabled = false;
-      //grupos.disabled = false;
-      //movimentacoes.disabled = false;
-      if (e.target.value == 'Empréstimo') {
-        //grupos.disabled = false;
-        //movimentacoes.disabled = true;
-        for (let i = 0; i < itens.children.length; i++)
-          itens.children[i].disabled = itens.children[i].className == 'ndisp';
-      } else {
-        //grupos.disabled = true;
-        //movimentacoes.disabled = false;
-        for (let i = 0; i < itens.children.length; i++)
-          itens.children[i].disabled = itens.children[i].className == 'disp';
-      }
-    }*/
-    if (e.target.value != '') {
-      if (e.target.value == 'Empréstimo')
-        for (let i = 0; i < itens.children.length; i++)
-          itens.children[i].disabled = itens.children[i].className == 'ndisp';
-      else
-        for (let i = 0; i < itens.children.length; i++)
-          itens.children[i].disabled = itens.children[i].className == 'disp';
-    }
-  }
-
-  function atualizarItens(e) {
-    //console.log(e.target.children[0].selected);
-    let arr = []; //pra converter HTMLCollection em Array, pra usar forEach
-    for (let i = 0; i < e.target.children.length; i++) {
-      arr.push(e.target.children[i]);
-    }
-    //console.log(arr);
-    arr.forEach(op => {
-      if (document.getElementById(op.value).value) {
-        let ids_dos_itens = document.getElementById(op.value).value.split(',');
-        //console.log(ids_dos_itens);
-        if (ids_dos_itens.length)
-          ids_dos_itens.forEach(id => {
-            if (op.selected && !document.getElementById(id).disabled)
-              document.getElementById(id).selected = op.selected;
-          });
-      }
-    });
+  function checar_itens(e) {
+    e.preventDefault();
+    if (document.getElementsByName('itens[]').length == 0)
+      document.getElementById('erro').textContent = 'Inclua pelo menos um item.';
+    else
+      e.target.submit();
   }
 </script>
 
